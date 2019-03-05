11bba5c9a1863123d23c2c0aec1e70f0ec3213a6
Lavanya Saravanan <lavanya.saravanan-1@ou.edu>
2019-03-05 16:27:01 -0600

    add post and display post done

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 import NotFound from "./components/notfound/NotFound";
 import "./App.css";
 
@@ -88,6 +89,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
