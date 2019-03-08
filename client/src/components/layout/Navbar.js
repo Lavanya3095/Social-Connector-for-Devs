c99766ea67f076297b99fb66f4fe9ef1e2618695
Lavanya Saravanan <lavanya.saravanan-1@ou.edu>
2019-03-07 22:31:42 -0600

    changed navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -66,7 +66,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevConnector
+            DevsConnect
           </Link>
           <button
             className="navbar-toggler"
