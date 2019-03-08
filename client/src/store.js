3a7d0f653c777805f78d5234d39c1fb0ed4221a7
Lavanya Saravanan <lavanya.saravanan-1@ou.edu>
2019-03-07 22:24:33 -0600

    changed for deployment redux tools

--- client/src/store.js
+++ client/src/store.js
@@ -4,13 +4,22 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+
+/*const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-);
+);*/
 
 export default store;
